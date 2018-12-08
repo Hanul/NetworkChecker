3d1d90f00dc085b139f267d7866c67a7fcc2c94d
YJ <hanul@hanul.me>
2018-12-08 23:03:03 +0900

    Solc 0.5.1 반영

--- NetworkChecker.sol
+++ NetworkChecker.sol
@@ -1,18 +1,18 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.1;
 
 // 숫자 계산 시 오버플로우 문제를 방지하기 위한 라이브러리
 contract NetworkChecker {
 	
-	address constant private MAINNET_MILESTONE_ADDRESS = 0x80c53F6b4920355659885304bE52b338ef4d56A8;
-	address constant private ROPSTEN_MILESTONE_ADDRESS = 0x80cF3dAF64cb4284214913ed5F6eb33Acfb9b9F6;
-	address constant private RINKEBY_MILESTONE_ADDRESS = 0xe9e046192a2FF2c2E4D1FE6AE6579279D50a83D3;
-	address constant private KOVAN_MILESTONE_ADDRESS = 0x52f9d987ABE3f12408e15E25389b17289289B50d;
+	address constant private MAINNET_MILESTONE_ADDRESS = 0xa6e90A28F446D3639916959B6087F68D9B83fca9;
+	address constant private KOVAN_MILESTONE_ADDRESS = 0x9a6Dc2a58256239500D96fb6f13D73b70C3d88f9;
+	address constant private ROPSTEN_MILESTONE_ADDRESS = 0x212cC55dd760Ec5352185A922c61Ac41c8dDB197;
+	address constant private RINKEBY_MILESTONE_ADDRESS = 0x54d1991a37cbA30E5371F83e8c2B1F762c7096c2;
 	
 	enum Network {
 		Mainnet,
+		Kovan,
 		Ropsten,
 		Rinkeby,
-		Kovan,
 		Unknown
 	}
 	
@@ -28,28 +28,42 @@ contract NetworkChecker {
 	constructor() public {
 		
 		// Main 네트워크인지 확인합니다.
-		if (checkIsSmartContract(MAINNET_MILESTONE_ADDRESS) == true && MAINNET_MILESTONE_ADDRESS.call(bytes4(keccak256("helloMainnet()"))) == true) {
-			network = Network.Mainnet;
-		}
-		
-		// Ropsten 네트워크인지 확인합니다.
-		else if (checkIsSmartContract(ROPSTEN_MILESTONE_ADDRESS) == true && ROPSTEN_MILESTONE_ADDRESS.call(bytes4(keccak256("helloRopsten()"))) == true) {
-			network = Network.Ropsten;
-		}
-		
-		// Rinkeby 네트워크인지 확인합니다.
-		else if (checkIsSmartContract(RINKEBY_MILESTONE_ADDRESS) == true && RINKEBY_MILESTONE_ADDRESS.call(bytes4(keccak256("helloRinkeby()"))) == true) {
-			network = Network.Rinkeby;
+		if (checkIsSmartContract(MAINNET_MILESTONE_ADDRESS) == true) {
+			(bool success, ) = MAINNET_MILESTONE_ADDRESS.call(abi.encodeWithSignature("helloMainnet()"));
+			if (success == true) {
+				network = Network.Mainnet;
+				return;
+			}
 		}
 		
 		// Kovan 네트워크인지 확인합니다.
-		else if (checkIsSmartContract(KOVAN_MILESTONE_ADDRESS) == true && KOVAN_MILESTONE_ADDRESS.call(bytes4(keccak256("helloKovan()"))) == true) {
-			network = Network.Kovan;
+		if (checkIsSmartContract(KOVAN_MILESTONE_ADDRESS) == true) {
+			(bool success, ) = KOVAN_MILESTONE_ADDRESS.call(abi.encodeWithSignature("helloKovan()"));
+			if (success == true) {
+				network = Network.Kovan;
+				return;
+			}
+		}
+		
+		// Ropsten 네트워크인지 확인합니다.
+		if (checkIsSmartContract(ROPSTEN_MILESTONE_ADDRESS) == true) {
+			(bool success, ) = ROPSTEN_MILESTONE_ADDRESS.call(abi.encodeWithSignature("helloRopsten()"));
+			if (success == true) {
+				network = Network.Ropsten;
+				return;
+			}
+		}
+		
+		// Rinkeby 네트워크인지 확인합니다.
+		if (checkIsSmartContract(RINKEBY_MILESTONE_ADDRESS) == true) {
+			(bool success, ) = RINKEBY_MILESTONE_ADDRESS.call(abi.encodeWithSignature("helloRinkeby()"));
+			if (success == true) {
+				network = Network.Rinkeby;
+				return;
+			}
 		}
 		
 		// 알 수 없는 네트워크
-		else {
-			network = Network.Unknown;
-		}
+		network = Network.Unknown;
 	}
 }
